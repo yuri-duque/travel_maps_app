92439af2b94f0c297dae5c36b34d07c0d08dcd47
yuri-duque <yurithielmann83@gmail.com>
2023-11-17 17:07:38 -0300

    creating HR component

---
+++ src/components/uiElements/hr/Hr.tsx
@@ -0,0 +1,21 @@
+import { View } from "native-base";
+import React from "react";
+
+export type HRProps = {
+  px?: number;
+  color?: string;
+  width?: number;
+};
+
+export default function HR({ px, color = "#ccc", width = 0.7 }: HRProps) {
+  return (
+    <View px={px}>
+      <View
+        style={{
+          borderBottomColor: color,
+          borderBottomWidth: width,
+        }}
+      />
+    </View>
+  );
+}
